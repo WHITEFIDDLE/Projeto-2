69b402843a7ae37cc0e6acbb89a640d17c3b0548
WHITEFIDDLE <lucas06chaves@gmail.com>
2026-07-28 22:19:02 -0300

    quarta tentativa

--- teste.py
+++ teste.py
@@ -36,4 +36,4 @@ if __name__ == "__main__":
     expr = input("Digite uma expressão: ")
     print(unsafe_eval(expr))
     
-print("Ola mundo hahahahaha que loucura hahahahahah")
+print("Ola mundo hahahahaha que loucura hahahahahah já estamos em 5")
